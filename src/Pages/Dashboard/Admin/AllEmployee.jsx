3bcfc7bf01f41240c7b31d1da909da76bdb89bca
Saif-Rahman-801 <saifurs102@gmail.com>
2023-12-01 22:49:26 +0600

    employee fire system added

--- src/Pages/Dashboard/Admin/AllEmployee.jsx
+++ src/Pages/Dashboard/Admin/AllEmployee.jsx
@@ -1,57 +1,74 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { toast } from "react-toastify";
 
 const AllEmployee = () => {
-  const [users, setUsers] = useState([]);
+  const [AllUsers, setAllUsers] = useState([]);
   const axiosPublic = useAxiosPublic();
 
   const fetchUser = () => {
     fetch("http://localhost:5000/users")
       .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((data) => setAllUsers(data))
       .catch((error) => console.error("Error fetching users:", error));
   };
+
+  const employees = AllUsers.filter((user) => user.isVerified === true);
 
   useEffect(() => {
     fetchUser();
   }, []);
 
+  const handleFire = (userId) => {
+    // console.log(userId);
+    axiosPublic.delete(`/users/${userId}`)
+      .then((res) => {
+        console.log(res.data);
+        if(res.data.deletedCount > 0){
+            toast.success("User fired successfully, It will be deleted after a refresh");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-<div className="overflow-x-auto">
-  <table className="w-full table-auto border-collapse">
-    <thead className="text-lg bg-gray-200">
-      <tr>
-        <th className="border px-4 py-2"></th>
-        <th className="border px-4 py-2">Name</th>
-        <th className="border px-4 py-2">Designation</th>
-        <th className="border px-4 py-2">Make HR</th>
-        <th className="border px-4 py-2">Fire</th>
-      </tr>
-    </thead>
-    <tbody className="font-medium">
-      {users.map((employee, idx) => (
-        <tr key={employee._id} className="border">
-          <td className="border px-4 py-2">{idx + 1}</td>
-          <td className="border px-4 py-2">{employee.name}</td>
-          <td className="border px-4 py-2">{employee.designation}</td>
+    <div className="overflow-x-auto">
+      <table className="w-full table-auto border-collapse">
+        <thead className="text-lg bg-gray-200">
+          <tr>
+            <th className="border px-4 py-2"></th>
+            <th className="border px-4 py-2">Name</th>
+            <th className="border px-4 py-2">Designation</th>
+            <th className="border px-4 py-2">Make HR</th>
+            <th className="border px-4 py-2">Fire</th>
+          </tr>
+        </thead>
+        <tbody className="font-medium">
+          {employees.map((employee, idx) => (
+            <tr key={employee._id} className="border">
+              <td className="border px-4 py-2">{idx + 1}</td>
+              <td className="border px-4 py-2">{employee.name}</td>
+              <td className="border px-4 py-2">{employee.designation}</td>
 
-          <td className="border px-4 py-2">
-            <button className="px-3 py-2 bg-green-500 rounded-md text-white">
-              Make HR
-            </button>
-          </td>
-          <td className="border px-4 py-2">
-            <button className="px-3 py-2 bg-red-500 rounded-md text-white">
-              Fire
-            </button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
+              <td className="border px-4 py-2">
+                <button className="px-3 py-2 bg-green-500 rounded-md text-white">
+                  Make HR
+                </button>
+              </td>
+              <td className="border px-4 py-2">
+                <button
+                  onClick={() => handleFire(employee._id)}
+                  className="px-3 py-2 bg-red-500 rounded-md text-white"
+                >
+                  Fire
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
